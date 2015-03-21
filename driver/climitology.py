correct mistakes in mixing length calculation

driver/climitology.py
@@ -1,25 +1,70 @@
 import qg_transform
 import nc_tools
-import matplotlib.pyplot as plt
 import numpy as np
+import diag
 
-filename_prefix = 'Jan17_drag_1e-4'
-save_dir = '/home/j1c/analysis/2015/qg_model/%s/' %filename_prefix
-psi = nc_tools.ncread('/archive/Junyi.Chai/QG_exp/%s' %filename_prefix,
-                      '%s_seg(180)' %filename_prefix,'psi')[:]
-psi = qg_transform.real2complex(psi)
-                      
-## zonal mean zonal wind                      
-
-uk, vk = qg_transform.get_velocities(psi)
-ug  = qg_transform.spec2grid(uk)
-
-mean_ug = np.mean(np.mean(np.mean(ug, -1), -1), 0)
-
-## zonal mean PV
+# Sc = 1.6, F = 1583.14, beta = 3957.86
+# Sc = 2.0, F = 1583.14, beta = 3166.29
+# Sc = 2.5, F = 1583.14, beta = 2533.03
 F    = 1583.14
 beta = 3957.9
-pvg = qg_transform.spec2grid(qg_transform.get_PV(psi, F))
-pvg += qg_transform.get_betay(pvg, beta)
 
-mean_pvg = np.mean(np.mean(np.mean(pvg, -1), -1), 0)
+filename_prefix = 'Jan17_drag_1e-2'
+save_dir = '/home/j1c/analysis/2015/qg_model/%s/' %filename_prefix
+psi = nc_tools.ncread('/archive/Junyi.Chai/QG_exp/%s' %filename_prefix,
+                      '%s_seg(24|25)' %filename_prefix,'psi')[:]
+psi = qg_transform.real2complex(psi)
+
+mean_u     = diag.zonal_mean_zonal_wind(psi)
+mean_pv    = diag.zonal_mean_PV(psi, F, beta)
+mean_dpvdy = diag.zonal_mean_dPVdy(psi, F, beta)
+
+## save as npz
+import matlab_style as mlab
+import os
+
+if not os.path.isdir(save_dir):
+    os.mkdir(save_dir)
+
+mlab.save(save_dir + 'zonal_mean_field', mean_u=mean_u, mean_pv=mean_pv, 
+                                         mean_dpvdy=mean_dpvdy)
+
+## plot the figures
+import matplotlib.pyplot as plt
+load_from_npz = False
+
+if load_from_npz:
+    import matlab_style as mlab
+    import numpy as np
+    filename_prefix = 'Jan17_drag_1e0'
+    save_dir = '/home/j1c/analysis/2015/qg_model/%s/' %filename_prefix
+    mlab.load(save_dir + 'zonal_mean_field.npz')
+
+import matplotlib
+font = {'size'   : 12}
+matplotlib.rc('font', **font)
+
+def plot_zonal_field(mean_field, title=None, figsize=(4, 6)):
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    y = np.linspace(-np.pi, np.pi, mean_field.shape[0])
+    if len(mean_field.shape) == 2 and mean_field.shape[1] != 1:
+        ax.plot(mean_field[:,0], y, label='upper')
+        ax.plot(mean_field[:,1], y, label='lower')
+    else:
+        ax.plot(mean_field, y)
+    ax.set_ylabel('y')
+    ax.legend(loc='best')
+    if title:
+        ax.set_title(title)
+    return fig
+    
+fig_u  = plot_zonal_field(mean_u, 'u')
+fig_pv = plot_zonal_field(mean_pv, 'pv')
+fig_dpvdy   = plot_zonal_field(mean_dpvdy, 'dPVdy')
+fig_dpvdybt = plot_zonal_field(np.mean(mean_dpvdy, 1), 'barotropic dPVdy')
+
+fig_u.savefig(save_dir + 'mean_u.png')
+fig_pv.savefig(save_dir + 'mean_PV.png')
+fig_dpvdy.savefig(save_dir + 'mean_dPVdy.png')
+fig_dpvdybt.savefig(save_dir + 'mean_barotropic_dPVdy.png')

driver/mixing_length.py
@@ -1,77 +1,64 @@
 """
-Calculate std_v, heat flux, tracer flux for evaluating mixing length
+Calculate square_v, heat flux, tracer flux for evaluating mixing length
 
-std_v     = <v, v>, <> denote domain averaged integral
+square_v  = <v, v>, <> denote domain averaged integral
 heat_flux = <v, \tau>, \tau is baroclinic stream function 
                        0.5*(phi_upper-phi_lower)
 tracer_flux = <v, tracer_y>
 """
 
 import numpy as np
+import os
 import qg_transform
 from nc_tools import NetCDFChain
+import matlab_style as ml
 
 exp_list  = ['Jan17_drag_', 'Jan18_c2_drag_', 'Jan18_c2.5_drag_']
-cri_list  = ['1.6', '2.0', '2.5']
-drag_list = ['1e0', '1e-1', '1e-2', '1e-3']
+cri_list  = ['1.6', '2', '2.5']
+drag_list = ['1e0', '1e-1', '1e-2', '1e-3', '5e-4', '1e-4']
 arch_dir  = '/archive/Junyi.Chai/QG_exp/'
-AVE_PERIOD = 200
-U_BAR    = 1.
-ROSSBY_R = 2*np.pi/250
+save_dir  = '/home/j1c/analysis/2015/qg_model/scales'
+last_n_files = 2
 
-
-std_v       = np.zeros((len(cri_list), len(drag_list)))
+square_v    = np.zeros((len(cri_list), len(drag_list)))
+square_u    = np.zeros((len(cri_list), len(drag_list)))
 heat_flux   = np.zeros((len(cri_list), len(drag_list)))
 tracer_flux = np.zeros((len(cri_list), len(drag_list), 2))
 
-def cal_time_ave_std(prod_int):
-    return np.std(np.mean(prod_int, 0))
 
 for i, exp_i in enumerate(exp_list):
     for j, drag_i in enumerate(drag_list):
         filedir = arch_dir + exp_i + drag_i
-        filename = r'%sseg[0-9]+' %(exp_i + drag_i + '_')
+        filename = r'%sseg[0-9]+[0-9]+' %(exp_i + drag_i + '_')
         print filename
         
-        psik = NetCDFChain(filedir, filename, 'psi')[-AVE_PERIOD:]
+        psik = NetCDFChain(filedir, filename, 'psi', last_n_files=last_n_files)[:]
         psik = qg_transform.real2complex(psik)
-        _, vk   = qg_transform.get_velocities(psik)
+        bt_uk, vk   = qg_transform.get_velocities(psik)
+        bt_uk = np.mean(bt_uk, -1)
         bt_vk = np.mean(vk, -1) #barotropic meridional velocity
-        std_v[i,j] = cal_time_ave_std(
-            qg_transform.prod_domain_ave_int(bt_vk, bt_vk))
+        square_v[i,j] = np.mean(
+            qg_transform.prod_domain_ave_int(bt_vk, bt_vk), 0)
+
+        square_u[i,j] = np.mean(
+            qg_transform.prod_domain_ave_int(bt_uk, bt_uk), 0)
+        del bt_uk
         
         tau = 0.5*(psik[...,0] - psik[...,1])
-        heat_flux[i,j] = cal_time_ave_std(
-            qg_transform.prod_domain_ave_int(bt_vk, tau))
+        heat_flux[i,j] = np.mean(
+            qg_transform.prod_domain_ave_int(bt_vk, tau), 0)
+        del tau
+        del psik
+        del bt_vk
             
-        tracer_yk = NetCDFChain(filedir, filename, 'tracer_y')[-AVE_PERIOD:]
+        tracer_yk = NetCDFChain(filedir, filename, 'tracer_y', last_n_files=last_n_files)[:]
         tracer_yk = qg_transform.real2complex(tracer_yk)
-        tracer_flux[i,j,0] = cal_time_ave_std(
-            qg_transform.prod_domain_ave_int(vk[...,0], tracer_yk[...,0]))
-        tracer_flux[i,j,1] = cal_time_ave_std(
-            qg_transform.prod_domain_ave_int(vk[...,1], tracer_yk[...,1]))
-            
-## calculate eddy scale and jet scale
-# ------------------------------------------------------------------------------
-# eddy scale is defined as (inverse) centroid of EKE
-# jet scale is defined as (inverse) centroid of ZKE
-# ------------------------------------------------------------------------------
-import os
-import diag
-analysis_dir = '/home/j1c/analysis/2015/qg_model'
+        tracer_flux[i,j,:] = np.mean(
+            qg_transform.prod_domain_ave_int(vk, tracer_yk), 0)
 
-cent_EKE = np.zeros((len(cri_list), len(drag_list)))
-cent_ZKE = np.zeros((len(cri_list), len(drag_list)))
+        del tracer_yk, vk
 
-for i, exp_i in enumerate(exp_list):
-    for j, drag_i in enumerate(drag_list):
-        exp_name = exp_i + drag_i
-        file_Ek = os.path.join(analysis_dir, exp_name, 'Ek.npz')
-        Ekzip = np.load(file_Ek)
-        k    = Ekzip['k']
-        EKEk = Ekzip['EKEk']
-        Ek   = Ekzip['Ek']
-        ZKEk = Ek - EKEk
-        
-        cent_EKE[i,j] = diag.centroid(k, EKEk)
-        cent_ZKE[i,j] = diag.centroid(k, ZKEk)
+        ml.save(os.path.join(save_dir, 'energy_scale.npz'), square_v=square_v,
+                                                    square_u=square_u,
+                                                    heat_flux=heat_flux,
+                                                    tracer_flux=tracer_flux) 

lib/qg_transform.py
@@ -64,15 +64,46 @@ def prod_domain_ave_int(field1, field2):
         
     Args:
         field1, field2: numpy array returned from real2complex
-                        shape is (time_step(optional), ky, kx, z)
+                        shape is (time_step, ky, kx, z(optional))
         
     Returns:
         prod_int: numpy array, integral for each time step in each layer
-                  shape is (time_step(optional), z)
+                  shape is (time_step, z(optional))
     """
+    if field1.shape != field2.shape:
+        raise TypeError("field1 and field2 don't have same shape")
     prod_int = np.real(field1*np.conj(field2))
-    prod_int = np.sum(np.sum(prod_int, -2), -2)
+    if len(field1.shape) == 4:
+        prod_int = np.sum(np.sum(prod_int, -2), -2)
+    elif len(field1.shape) == 3:
+        prod_int = np.sum(np.sum(prod_int, -1), -1)
+    else:
+        raise NotImplementedError("unknown shape")
     return prod_int
+    
+def prod_spectrum(field1, field2):
+    """
+    Spectrum of product field1*field2. Intend for flux v'*\tau
+    """
+    if field1.shape != field2.shape:
+        raise TypeError('field1 and field2 have different shapes')
+    
+    prod2d = np.real(field1*np.conj(field2))
+    prod2d_ave = np.mean(prod2d, 0)
+        
+    nky, nkx = field1.shape[-3:-1]
+    kmax = nky - 1
+    kz = field1.shape[-1]
+    ksqd_ = np.zeros((nky, nkx), dtype=float)
+    for j in range(0, nky):
+        for i in range(0, nkx):
+            ksqd_[j,i] = (i-kmax)**2 + j**2
+    radius_arr = np.floor(np.sqrt(ksqd_)).astype(int)
+
+    spec1d = np.zeros((kmax, kz))
+    for i in range(0,kmax):
+        spec1d[i,:]   = np.sum(prod2d_ave[radius_arr == i+1,:], 0)
+    return np.arange(1,kmax+1), spec1d
     
 def get_betay(pvg, beta):
     """