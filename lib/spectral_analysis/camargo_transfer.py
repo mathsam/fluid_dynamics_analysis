7f5ab07c6ce4dd586d582a90aedaae36d40751f7
Junyi Chai <junyic@princeton.edu>
2015-11-14 10:26:56 -0500

    fixed bugs in camargo_transfer

--- lib/spectral_analysis/camargo_transfer.py
+++ lib/spectral_analysis/camargo_transfer.py
@@ -11,12 +11,12 @@ class StandardSpectrum(object):
         self._spec = spec
         self._kxymax = spec.shape[-2]-1
     
-    def is_valid_mode(kx, ky):
+    def is_valid_mode(self, kx, ky):
         if abs(kx) > self._kxymax or abs(ky) > self._kxymax:
             return False
         return True
     
-    def get_mode(kx, ky):
+    def get_mode(self, kx, ky):
         """get complex component for mode (kx, ky)
         
         Args:
@@ -44,21 +44,54 @@ class CamargoTrans(object):
     See Manz, Ramisch and Stroth, 2009, Physical mechanism behind zonal-flow
     generation in drift-wave turbulence, Phys. Rev. Lett.
     """
-    def __init__(self, spec):
+    def __init__(self, spec, pre_cal=True, K_wall=50):
         """
         Args:
             spec: StandardSpectrum object or numpy array (not std spec)
+            pre_cal: bool|numpy array, whether to precalculate all the transfers within
+                      wavenumber shell `K_wall`
+                     if type is numpy array, just assign this array to be _transM
+            K_wall: integer
         """
         if isinstance(spec, StandardSpectrum):
             self._stdspec = spec
         else:
             self._stdspec = StandardSpectrum(spec)
+        
+        if isinstance(pre_cal, bool):
+            if pre_cal:
+                num_ks = 2*K_wall + 1
+                self._transM = np.zeros((num_ks, num_ks, num_ks, num_ks)) #(kx1, ky1, kx, ky)
+                kx1_set = range(-K_wall, K_wall+1)
+                ky1_set = range(-K_wall, K_wall+1)
+                kx_set  = range(-K_wall, K_wall+1)
+                ky_set  = range(-K_wall, K_wall+1)
+                for i1, kx1 in enumerate(kx1_set):
+                    for j1, ky1 in enumerate(ky1_set):
+                        print "calculating kx1=%d, ky1=%d" %(kx1, ky1)
+                        for i, kx in enumerate(kx_set):
+                            for j, ky in enumerate(ky_set):
+                                self._transM[i1,j1,i,j] = CamargoTrans._cal_transfer(
+                                    self._stdspec, (kx1, ky1), (kx, ky))
+                self._if_pre_cal = True
+                self._K_wall = K_wall
+        elif isinstance(pre_cal, np.ndarray):
+            num_kxs, num_kys = pre_cal.shape[:2]
+            if num_kxs == num_kys:
+                self._transM = pre_cal.copy()
+                self._K_wall = (num_kxs-1)/2
+            else:
+                raise IndexError('pre_cal array has wrong shape')
+        else:
+            raise TypeError('pre_cal has wrong type')
     
-    def get_transfer(K1, K):
+    @staticmethod
+    def _cal_transfer(stdspec, K1, K):
         """
         Args:
+            stdspec: StandardSpectrum object
             K1: tuple for (k1x, k1y)
-            K:  tuple for (kx, ky)
+            K: tuple for (kx, ky)
         
         Return:
             a float
@@ -68,12 +101,29 @@ class CamargoTrans(object):
         k2x = kx - k1x
         k2y = ky - k1y
         
-        if not self._stdspec.is_valid_mode(k2x, k2y):
+        if not stdspec.is_valid_mode(k2x, k2y):
             raise IndexError('interaction not exist')
         
-        psi_K1 = self._stdspec.get_mode(k1x, k1y)
-        psi_K2 = self._stdspec.get_mode(k2x, k2y)
-        psi_K  = self._stdspec.get_mode(kx, ky)
+        psi_K1 = stdspec.get_mode(k1x, k1y)
+        psi_K2 = stdspec.get_mode(k2x, k2y)
+        psi_K  = stdspec.get_mode(kx, ky)
         
         return -2.*(ky*k1x - k1y*kx)*(k2x**2+k2y**2)*np.mean(
-            np.real(np.conj(psi_K) * psi_K2 * psi_K1)) )
+            np.real(np.conj(psi_K) * psi_K2 * psi_K1) )
+        
+    
+    def get_transfer(self, K1, K):
+        """
+        Args:
+            K1: tuple for (k1x, k1y)
+            K:  tuple for (kx, ky)
+        
+        Return:
+            a float
+        """
+        if hasattr(self, '_transM'):
+            k1x, k1y = K1
+            kx, ky   = K
+            return self._transM[k1x+self._K_wall, k1y+self._K_wall,
+                                kx+self._K_wall,  ky+self._K_wall]
+        return CamargoTrans._cal_transfer(self._stdspec, K1, K)
